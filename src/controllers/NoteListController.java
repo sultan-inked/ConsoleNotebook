7037613304337b221464a748a21217772841ad4b
Sultan <nikowosuka@gmail.com>
2024-03-17 15:07:27 +0500

    MOD: Delete never used strings in NoteListController.java

--- src/controllers/NoteListController.java
+++ src/controllers/NoteListController.java
@@ -1,13 +1,10 @@
 package controllers;
 
 import java.io.BufferedReader;
-//import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-//import java.time.LocalDateTime;
-//import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import models.Note;
@@ -37,12 +34,6 @@ public class NoteListController {
 	}
 	
 //	Methods:
-//	public void saveTemporaryNote() {
-//		File fileTemp = new File("notes/temp.TXT");
-//		fileNameIteration();
-//		File file = new File("notes/" + fileNameNumber + ".TXT");
-//		fileTemp.renameTo(file);
-//	}
 	public void fileNameIteration() {
 		try(var fileWriter = new FileWriter("notes/fileCounter.TXT")){
 			fileNameNumber++;
@@ -54,56 +45,6 @@ public class NoteListController {
 		}
 		refreshFileNameNumber();
 	}
-
-//	public void addFileInfoToNoteList() {
-//		var file = new File("notes/" + fileNameNumber + ".TXT");
-//		String newFileInfo = getFileInfo(file) + "\r\n";
-//		var fileTemp = new File("notes/temp.TXT");
-//		var fileListOfNotes = new File("notes/ListOfNotes.TXT");
-//		
-//		try(var fileWriter = new FileWriter(fileTemp, true);
-//			var bufferedReader = new BufferedReader(new FileReader(fileListOfNotes))){
-//			fileWriter.write(newFileInfo);
-//			String str;
-//			do {
-//				str = bufferedReader.readLine();
-//				if(str != null) {
-//					str += "\r\n";
-//					fileWriter.write(str);
-//				}
-//			}while(str != null);
-//		}
-//		catch(IOException exc) {
-//			System.out.println(" - I-O exception: " + exc);
-//		}
-//		
-//		fileListOfNotes.delete();
-//		fileTemp.renameTo(fileListOfNotes);
-//	}
-//	private String getFileInfo(File file) {
-//		return fileNameNumber + " " + getCurrentDateTime() + " " + getTitleOfNote(file).replaceAll(" ", "_");
-//	}
-//	private String getCurrentDateTime() {
-//		var currentDateTime = LocalDateTime.now();
-//		var formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy hh:mm:ss");
-//		String formattedDateTime = currentDateTime.format(formatter);
-//		
-//		return formattedDateTime;
-//	}
-//	private String getTitleOfNote(File file) {
-//		String str = "";
-//		try(var bufferedReader = new BufferedReader(new FileReader(file))){
-//			str = bufferedReader.readLine();
-//			System.out.println(" *** " + file);
-//			System.out.println(str);
-//			
-//		}
-//		catch(IOException exc) {
-//			System.out.println(" - I-O exception: " + exc);
-//		}
-//		System.out.println(str);
-//		return str;
-//	}
 	
 	private void refreshFileNameNumber() {
 		try(var bufferedReader = new BufferedReader(new FileReader("notes/fileCounter.TXT"))){
